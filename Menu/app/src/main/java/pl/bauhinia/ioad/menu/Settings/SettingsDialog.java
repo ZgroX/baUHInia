e4ccf43a26967e35cfd5625119860e1b795b2bdf
Przemysław Zdrzalik <224466@edu.p.lodz.pl>
2020-12-13 19:12:21 +0100

    dialog settings layout

--- Menu/app/src/main/java/pl/bauhinia/ioad/menu/Settings/SettingsDialog.java
+++ Menu/app/src/main/java/pl/bauhinia/ioad/menu/Settings/SettingsDialog.java
@@ -1,4 +1,28 @@
 package pl.bauhinia.ioad.menu.Settings;
 
-public class SettingsDialog {
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+
+public class SettingsDialog extends Dialog implements View.OnClickListener {
+
+    private Context context;
+
+    public SettingsDialog(@NonNull Context context) {
+        super(context);
+        this.context = context;
+    }
+
+    /**
+     * Called when a view has been clicked.
+     *
+     * @param v The view that was clicked.
+     */
+    @Override
+    public void onClick(View v) {
+
+    }
 }
