70c2c36ad9c1033f26ecb4dee1372031aec9201c
Przemysław Zdrzalik <224466@edu.p.lodz.pl>
2020-12-13 18:57:39 +0100

    view binding + makiety metod

--- Menu/app/src/main/java/pl/bauhinia/ioad/menu/Menu.java
+++ Menu/app/src/main/java/pl/bauhinia/ioad/menu/Menu.java
@@ -3,6 +3,7 @@ package pl.bauhinia.ioad.menu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class Menu extends AppCompatActivity {
 
@@ -11,4 +12,22 @@ public class Menu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void settingButtonOnClick(View v) {
+
+    }
+
+    public void loginButtonOnClick(View v) {
+
+    }
+
+    public void showMapListSButtonOnClick(View v) {
+
+    }
+
+    public void showElementEditorButtonOnClick(View v) {
+
+    }
+
+
 }
