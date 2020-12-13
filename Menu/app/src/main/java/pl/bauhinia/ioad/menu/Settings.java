918b6399ea57f0e3072462b9d2ac96e92670d07b
Przemysław Zdrzalik <224466@edu.p.lodz.pl>
2020-12-13 18:47:02 +0100

    set layout to view

--- Menu/app/src/main/java/pl/bauhinia/ioad/menu/Settings.java
+++ Menu/app/src/main/java/pl/bauhinia/ioad/menu/Settings.java
@@ -2,24 +2,37 @@ package pl.bauhinia.ioad.menu;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 
-public class Settings extends View {
+public class Settings extends FrameLayout {
     public Settings(Context context) {
         super(context);
+        initComponent(context);
     }
 
     public Settings(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        initComponent(context);
     }
 
     public Settings(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        initComponent(context);
     }
 
     public Settings(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
+        initComponent(context);
+    }
+
+    private void initComponent(Context context) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View v = inflater.inflate(R.layout.settings, this);
     }
 }
