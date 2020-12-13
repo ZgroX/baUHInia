ad4726b70528109c95f06a6cb84e76f83c9cc058
Przemysław Zdrzalik <224466@edu.p.lodz.pl>
2020-12-13 18:37:43 +0100

    layout settings

---
+++ Menu/app/src/main/java/pl/bauhinia/ioad/menu/Settings.java
@@ -0,0 +1,25 @@
+package pl.bauhinia.ioad.menu;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class Settings extends View {
+    public Settings(Context context) {
+        super(context);
+    }
+
+    public Settings(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Settings(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public Settings(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
