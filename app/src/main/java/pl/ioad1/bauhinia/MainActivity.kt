e1bbddc8f34cbcb6e1418738a2e672f3faaae8c7
Artur <artur300998@gmail.com>
2020-12-17 23:58:23 +0100

    Initial commit for the MapViewer application module. Sample tabbed activity.

--- app/src/main/java/pl/ioad1/bauhinia/MainActivity.kt
+++ app/src/main/java/pl/ioad1/bauhinia/MainActivity.kt
@@ -1,11 +1,21 @@
 package pl.ioad1.bauhinia
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import pl.ioad1.bauhinia.mapviewer.MapViewerMainActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        runActivity();
+    }
+
+    fun runActivity() {
+        val intent = Intent(this, MapViewerMainActivity::class.java);
+        startActivity(intent);
     }
 }
