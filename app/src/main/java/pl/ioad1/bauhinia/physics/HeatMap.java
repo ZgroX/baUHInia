5a0a8c60e810ec2629a14ffbfa911817bbe9d9bb
Kamil Skalski <224427@edu.p.lodz.pl>
2020-12-18 19:07:41 +0100

    Init for physics

---
+++ app/src/main/java/pl/ioad1/bauhinia/physics/HeatMap.java
@@ -0,0 +1,15 @@
+package pl.ioad1.bauhinia.physics;
+
+public class HeatMap {
+
+    public double SunImpact() {
+        return 0;
+    }
+
+    public  double ElementImpact() {
+        return 0;
+    }
+
+    public void generateHeatMap() { }
+
+}
---
+++ app/src/main/java/pl/ioad1/bauhinia/physics/MapValidation.java
@@ -0,0 +1,17 @@
+package pl.ioad1.bauhinia.physics;
+
+public class MapValidation {
+
+    public boolean isValidated() {
+        return false;
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public boolean isCorrect() {
+        return false;
+    }
+
+}
