ffba0bd58e910249635c9343cb8fefdd0c0dc43c
Hubert Gawlowski <hubertgaw@gmail.com>
2020-12-18 15:17:48 +0100

    Init MapEditor

--- app/src/main/java/pl/ioad1/bauhinia/MainActivity.kt
+++ app/src/main/java/pl/ioad1/bauhinia/MainActivity.kt
@@ -1,11 +1,20 @@
 package pl.ioad1.bauhinia
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import pl.ioad1.bauhinia.mapeditor.MapEditorMainActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        runActivity();
+    }
+
+    fun runActivity() {
+        val intent = Intent(this, MapEditorMainActivity::class.java);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/pl/ioad1/bauhinia/mapeditor/MapEditorMainActivity.java
@@ -0,0 +1,16 @@
+package pl.ioad1.bauhinia.mapeditor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import pl.ioad1.bauhinia.R;
+
+public class MapEditorMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_editor_main);
+    }
+}
