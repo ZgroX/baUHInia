d95e83b59cb8a7394f1a2ca37f311cef5e253ad0
jakubik <224307@edu.p.lodz.pl>
2020-12-20 12:53:41 +0100

    first upload
    Not working connection with server

---
+++ app/src/main/java/pl/ioad1/bauhinia/elementeditor/ElementEditorCommunicator.java
@@ -0,0 +1,55 @@
+package pl.ioad1.bauhinia.elementeditor;
+
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+
+import java.util.ArrayList;
+
+
+public class ElementEditorCommunicator {
+//    private ElementProvider elementProvider;
+    private ArrayList<Element> elements = new ArrayList<>();
+
+    ArrayList<Element> dowloadList(){
+        reloadList();
+        return elements;
+    }
+    Element getElementById(int id){
+        reloadList();
+        for(Element element : elements){
+            if(id==element.getId())
+                return element;
+        }
+        return null;
+    }
+    boolean saveElementChange(Element elementToSave){
+        boolean ret=true;
+
+        return ret;
+    }
+    boolean addElement(Element elementToSave){
+        boolean ret=true;
+
+        return ret;
+    }
+    private void reloadList(){
+        elements.clear();
+
+        for(int i = 1 ; i < 21 ; i++)
+            elements.add(new Element(i,i,i,"Name"+i, null ,i,i));
+    }
+
+    public int generateId() {
+        int id = 1;
+        reloadList();
+        while(true){
+            for(Element element : elements){
+                if(id!=element.getId())
+                    return id;
+                id++;
+            }
+        }
+
+    }
+}
+
