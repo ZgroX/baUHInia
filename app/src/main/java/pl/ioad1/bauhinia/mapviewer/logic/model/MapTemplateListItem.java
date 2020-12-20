c8de221c1d75e4a3a821127a8ca987be02926677
bruderooo <szymonjacon@gmail.com>
2020-12-20 22:43:46 +0100

    Changes in MapTemplateListItem and in methods which use it.

--- app/src/main/java/pl/ioad1/bauhinia/mapviewer/logic/model/MapTemplateListItem.java
+++ app/src/main/java/pl/ioad1/bauhinia/mapviewer/logic/model/MapTemplateListItem.java
@@ -1,23 +1,31 @@
 package pl.ioad1.bauhinia.mapviewer.logic.model;
 
+import java.util.List;
+
+import pl.ioad1.bauhinia.elementeditor.Element;
+import pl.ioad1.bauhinia.sessionManager.data.PlacedElement;
+
 public class MapTemplateListItem {
     private int id;
     private String name;
     private String district;
     private int maxBudget;
+    private List<PlacedElement> placedElements;
 
-    public MapTemplateListItem(int id, String name, String district, int maxBudget) {
+    public MapTemplateListItem(int id, String name, String district, int maxBudget, List<PlacedElement> placedElements) {
         this.id = id;
         this.name = name;
         this.district = district;
         this.maxBudget = maxBudget;
+        this.placedElements = placedElements;
     }
 
-    public MapTemplateListItem(Object id, Object name, Object district, Object maxBudget) {
+    public MapTemplateListItem(Object id, Object name, Object district, Object maxBudget, Object placedElements) {
         this.id = (int) id;
         this.name = (String) name;
         this.district = (String) district;
         this.maxBudget = (int) maxBudget;
+        this.placedElements = (List<PlacedElement>) placedElements;
     }
 
     public int getId() {
@@ -34,5 +42,17 @@ public class MapTemplateListItem {
 
     public int getMaxBudget() {
         return maxBudget;
+    }
+
+    public void addPlacedElement(PlacedElement placedElement) {
+        this.placedElements.add(placedElement);
+    }
+
+    public void addPlacedElement(Element element, int x, int y) {
+        this.placedElements.add(new PlacedElement(element, x, y));
+    }
+
+    public List<PlacedElement> getPlacedElements() {
+        return placedElements;
     }
 }
